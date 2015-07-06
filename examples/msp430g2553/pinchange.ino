475f50ad37a35364c30969c66932f37cb43d2331
Stephen Crane <jscrane@gmail.com>
2015-07-06 16:11:49 +0100

    millisecond timer!

---
+++ examples/msp430g2553/pinchange.ino
@@ -0,0 +1,32 @@
+#include <Interrupted.h>
+
+PinChangeGroup p1;
+PinChange led(RED_LED, p1);
+PinChange push2(PUSH2, p1);
+Devices devices;
+
+void setup()
+{
+	pinMode(RED_LED, OUTPUT);
+	pinMode(GREEN_LED, OUTPUT);
+	pinMode(PUSH2, INPUT_PULLUP);
+	digitalWrite(RED_LED, LOW);
+	digitalWrite(GREEN_LED, HIGH);
+
+	devices.add(led);
+	devices.add(push2);
+	devices.begin();
+}
+
+void loop()
+{
+	switch (devices.select()) {
+	case RED_LED:
+		digitalWrite(GREEN_LED, !digitalRead(GREEN_LED));
+		break;
+	case PUSH2:
+		if (!push2.is_on())
+			digitalWrite(RED_LED, !digitalRead(RED_LED));	
+		break;
+	}
+}
--- examples/msp430g2553/useless.ino
+++ examples/msp430g2553/useless.ino
@@ -1,32 +1,40 @@
 #include <Interrupted.h>
 
+/*
+ * The LED comes on for 1.5 seconds after reset or when the
+ * pushbutton is pressed.
+ */
+#define TIMER	1
+
+Timer timer(TIMER, 1500);
 PinChangeGroup p1;
-PinChange led(RED_LED, p1);
+PinChange led(RED_LED, p1); 
 PinChange push2(PUSH2, p1);
 Devices devices;
 
-void setup()
+void setup(void)
 {
-	pinMode(RED_LED, OUTPUT);
-	pinMode(GREEN_LED, OUTPUT);
-	pinMode(PUSH2, INPUT_PULLUP);
-	digitalWrite(RED_LED, LOW);
-	digitalWrite(GREEN_LED, HIGH);
-
+	devices.add(timer);
 	devices.add(led);
 	devices.add(push2);
 	devices.begin();
+
+	pinMode(RED_LED, OUTPUT);
+	pinMode(PUSH2, INPUT_PULLUP);
 }
 
-void loop()
+void loop(void)
 {
 	switch (devices.select()) {
-	case RED_LED:
-		digitalWrite(GREEN_LED, !digitalRead(GREEN_LED));
-		break;
 	case PUSH2:
 		if (!push2.is_on())
-			digitalWrite(RED_LED, !digitalRead(RED_LED));	
+			digitalWrite(RED_LED, HIGH);
+		break;
+	case TIMER:
+		digitalWrite(RED_LED, LOW);
+		break;
+	case RED_LED:
+		timer.enable(led.is_on());
 		break;
 	}
 }
