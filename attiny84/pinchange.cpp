42d86d1c31b1325de4334f5867267bc44bc6daaf
Stephen Crane <jscrane@gmail.com>
2016-02-14 13:42:13 +0000

    update for damellis attiny core

--- attiny84/pinchange.cpp
+++ attiny84/pinchange.cpp
@@ -22,21 +22,12 @@ void Port::add_pin(int pin, Pin *p) {
 		_port = digitalPinToPort(pin);
 	byte b = digitalPinToBitMask(pin);
 	_pins[bit_index(b)] = p;
-	if (_port == PORTA) { 
-		PCMSK0 |= b; 
-		d[0] = this;
-	} else if (_port == PORTB) {
-		PCMSK1 |= b;
-		d[1] = this;
-	}
+	*(digitalPinToPCMSK(pin)) |= b;
+	d[_port - 1] = this;
 }
 
 void Port::enable_pin(int pin, bool enable) {
-	byte e = 0;
-	if (_port == PORTA)
-		e = bit(PCIE0);
-	else if (_port == PORTB)
-		e = bit(PCIE1);
+	byte e = bit(digitalPinToPCICRbit(pin));
 	byte b = digitalPinToBitMask(pin);
 	byte prev = _enabled;
 	if (enable) {
