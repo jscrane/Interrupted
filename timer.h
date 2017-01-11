42b731722ea856d91c36a9fae940b68708cdcd32
Ian Cavén <icaven@imageandvideo.com>
2017-01-11 10:41:28 -0800

    Change Timer interface to include sleep and wake functions.
    Also, allow a millisecond divisor to be specified explicitly.

--- timer.h
+++ timer.h
@@ -1,19 +1,28 @@
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
+#include "atimer.h"
+
 /**
  * A millisecond timer.
  */
 class Timer: public AbstractTimer {
 public:
-	Timer(int id, uint32_t millis): AbstractTimer(id, millis) {}
+	Timer(int id, uint32_t millis, uint32_t ms_divisor=1L):
+		AbstractTimer(id, millis), _ms_divisor(ms_divisor) {}
 
 	// not enabled by default
 	bool begin();
 
+	// virtual void ready();
+
+	virtual void sleep();
+	virtual void wake();
+
 protected:
 	void _enable(bool);
 	unsigned _sleepmode();
+	uint32_t _ms_divisor;
 };
 
 #endif
