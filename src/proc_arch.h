e278c4bec9234a8a9a7c51a200453cb7acaaa325
Stephen Crane <jscrane@gmail.com>
2017-09-28 10:31:17 +0100

    attiny85

--- src/proc_arch.h
+++ src/proc_arch.h
@@ -1,5 +1,7 @@
 #if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
 #define ARCH atmega328p
+#elif defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__)
+#define ARCH attiny85
 #else
 #error "Unknown arch!"
 #endif
