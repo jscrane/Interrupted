519acb34565e9e20c73751cb3977bc794073ba8b
Stephen Crane <jscrane@gmail.com>
2019-06-10 10:41:47 +0100

    fix rtc example on fr5739

--- examples/msp430fr5739/clock.ino
+++ examples/msp430fr5739/clock.ino
@@ -1,14 +1,87 @@
 #include <Interrupted.h>
-#include <clock.h>
 
 /*
  * PUSH1 for time-adjust mode, PUSH2 to advance hh/mm, PUSH1 to restart.
  * The RTC requires a 32.768kHz crystal.
  */
+class Clock: public Device {
+public:
+	Clock(int id): Device(id) {}
+
+	void set(uint8_t h, uint8_t m, uint8_t s) {
+		RTCSEC = s;
+		RTCMIN = m;
+		RTCHOUR = h;
+	}
+
+	// enabled by default
+	bool begin();
+
+	uint8_t hour() { return _h; }
+	uint8_t mins() { return _m; }
+	uint8_t secs() { return _s; }
+
+	void update(uint8_t h, uint8_t m, uint8_t s) {
+		_h = h; _m = m; _s = s;
+		ready();
+	}
+
+protected:
+	void _enable(bool);
+	unsigned _sleepmode() {
+		return LPM4_bits;
+	}
+
+private:
+	volatile uint8_t _h, _m, _s;
+};
+
+static Clock *c;
+
+// see https://e2e.ti.com/support/microcontrollers/msp430/f/166/t/118916
+bool Clock::begin() {
+	c = this;
+
+	PJSEL0 |= BIT4 | BIT5;			// XT1
+	CSCTL0_H = 0xa5;			// unlock register
+	CSCTL4 &= ~XT1OFF;
+	do {
+		CSCTL5 &= ~XT1OFFG;		// clear XT1 fault flag
+		SFRIFG1 &= ~OFIFG;
+	} while (SFRIFG1 & OFIFG);
+	CSCTL0_H = 0x01;			// lock register
+
+	return true;
+}
+
+void Clock::_enable(bool e) {
+	RTCCTL01 |= RTCHOLD;
+	if (e)
+		RTCCTL01 |= RTCRDYIE;
+	else
+		RTCCTL01 &= ~RTCRDYIE;
+	RTCCTL01 &= ~RTCHOLD;
+}
+
+#pragma vector=RTC_VECTOR
+__interrupt void rtc_isr(void)
+{
+	switch (RTCIV) {
+	case RTCIV_NONE:
+		break;
+	case RTCIV_RTCRDYIFG:
+		while (!(RTCRDY));
+		if (c)
+			c->update(RTCHOUR, RTCMIN, RTCSEC);
+		break;
+	}
+        __bic_SR_register_on_exit(LPM4_bits | GIE);
+}
+
 #define RTC	1
 #define SEROUT	99
 
-SerialOut output(SEROUT, 9600);
+SerialOut output(SEROUT, TERMINAL_SPEED);
 Clock clock(RTC);
 Port p4;
 Pin push1(PUSH1, p4);
@@ -32,7 +105,7 @@ void loop(void)
 {
 	static bool colon = false, hour_mode = false;
 	static char buf[16];
-	uint8_t h = clock.hour(), m = clock.mins();
+	uint8_t h = clock.hour(), m = clock.mins(), s = clock.secs();
 
 	switch (devices.select()) {
 	case RTC:
@@ -67,6 +140,6 @@ void loop(void)
 		return;
 	}
 	char c = colon? ':': ' ';
-	sprintf(buf, "%02d%c%02d\r", h, c, m);
+	sprintf(buf, "%02d%c%02d%c%02d\r", h, c, m, c, s);
 	output.write(buf);
 }
