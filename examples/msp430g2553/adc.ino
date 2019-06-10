acba20157d9c7185c6bd93c270af4b512bd8bb80
Stephen Crane <jscrane@gmail.com>
2019-06-10 10:17:32 +0100

    fix broken examples

--- examples/msp430g2553/adc.ino
+++ examples/msp430g2553/adc.ino
@@ -3,20 +3,32 @@
 /*
  * An analog input on pin 6, A4 (e.g., LDR/resistor voltage divider).
  */
+#define SEROUT  99
+
 Analog adc(A4, INTERNAL2V5);
+SerialOut output(SEROUT, TERMINAL_SPEED);
 Devices devices;
 
 void setup(void)
 {
 	devices.add(adc);
+	devices.add(output);
 	devices.begin();
-
-	Serial.begin(9600);
+	adc.enable();
 }
 
 void loop(void)
 {
-	adc.enable();
-	if (devices.select() == A4)
-		Serial.println(adc.read());
+	char buf[8];
+
+	switch (devices.select()) {
+	case A4:
+		itoa(adc.read(), buf, 10);
+		strcat(buf, "\r\n");
+		output.write(buf);
+		break;
+	case SEROUT:
+		adc.enable();
+		break;
+	}
 }
